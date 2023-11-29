7a72a30a0e5bdf56c19ac9d49d0fd18f2985a3f3
taniot <gaetano.frascolla@gmail.com>
2023-11-29 11:30:25 +0100

    feat: todo obj

--- js/main.js
+++ js/main.js
@@ -9,7 +9,20 @@ const myApp = createApp({
     return {
       newTask: '',
       keyTask: '',
-      tasks: ['Fare una passeggiata', 'Leggere un libro', 'Mangiare un gelato'],
+      tasks: [
+        {
+          text: 'Fare una passeggiata',
+          done: false,
+        },
+        {
+          text: 'Leggere un libro',
+          done: false,
+        },
+        {
+          text: 'Mangiare un gelato',
+          done: false,
+        },
+      ],
     };
   },
   methods: {
@@ -18,19 +31,42 @@ const myApp = createApp({
     },
     addTask() {
       if (this.newTask.trim() !== null) {
-        this.tasks.push(this.newTask);
+        // this.tasks.push(this.newTask);
+        this.tasks.push({
+          text: this.newTask,
+          done: false,
+        });
         this.newTask = '';
       }
     },
-
+    doneTask(index) {
+      console.log(index);
+      console.log("gestisco lo stato dell'evento");
+      this.tasks[index].done = !this.tasks[index].done;
+    },
+    //deve restituire sempre un array
     filteredTask() {
       if (this.keyTask.trim() !== '') {
-        return this.tasks.filter((task) =>
-          task.toLowerCase().includes(this.keyTask.toLowerCase())
-        );
+        return this.tasks
+          .filter((task) =>
+            task.text.toLowerCase().includes(this.keyTask.toLowerCase())
+          )
+          .map((task) => {
+            return {
+              ...task,
+              text: task.text + ' ciao',
+            };
+          });
       } else {
         return this.tasks;
       }
+      // return [
+      //   {
+      //     text: 'Mangiare un gelato',
+      //     done: false,
+      //   },
+      // ];
+      // return this.tasks;
     },
   },
 });
